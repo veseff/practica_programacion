024550a6366b7408de39efa4e1abf9bdfe1787b7
IvanQ <119110418+veseff@users.noreply.github.com>
2026-05-04 08:42:55 -0300

    Add files via upload

---
+++ practica_programacion.py
@@ -0,0 +1,23 @@
+#print("hola mundo") #imprime un resultado
+
+
+"""nombre=input("¿como te llamas?") #input permite la entrada de datos por teclado
+print("hola",nombre) """
+
+
+"""palabra=input("escribi un texto: ")
+print("cantidad de letras:", len(palabra)) #cuenta todos los caracteres: letras, signos de puntuacion, espacios"""
+
+
+"""numero=input("ingresa un numero: ")
+numero=int(numero) #convierte el numero en decimal
+print("el doble es:",numero*2)"""
+
+
+"""numero=float(input("ingresa un numero decimal: "))
+print("redondeado: ", round(numero)) #redondea los numeros decimales """
+
+
+nombre=input("ingresa tu nombre: ")
+edad=float(input("cual es tu edad: "))
+print(f"hola {nombre} tu nombre tiene: {len(nombre)} caracteres y tienes {round(edad)} años")
